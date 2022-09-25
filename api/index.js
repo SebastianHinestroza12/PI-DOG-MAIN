7d4262bef78f2b7eeb1f0b72f52b14681fd06a9c
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2022-09-24 23:06:31 -0500

    .env ok

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync().then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`Servidor Andando En El Puerto ${process.env.PORT} 🌐🟢`)
   });
