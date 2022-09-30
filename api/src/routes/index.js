851e7f41c0ea0b21e7f530d83bb1acb2c44caa6f
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2022-09-30 16:21:56 -0500

    error corregido

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -17,7 +17,7 @@ router.get('/dogs/:id', getDogsId);
 router.get('/temperaments', getTemperamento);
 router.post('/dogs', postDogs);
 router.delete('/dogs', deleteBd);
-router.update('/dogs', updateDb);
+router.put('/dogs', updateDb);
 
 
 module.exports = router;
