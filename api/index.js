0e3846a50186d3ac410be0dc275735a676da0eeb
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2022-10-23 00:12:18 -0500

    configuracion variables

--- api/index.js
+++ api/index.js
@@ -19,10 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || 3001;
+
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(port, () => {
     console.log(`Servidor Andando En El Puerto ${process.env.PORT} 🌐🟢`)
   });
 });
--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, API_KEY, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 } = process.env;
 
 let sequelize =
@@ -12,7 +12,7 @@ let sequelize =
       database: DB_NAME,
       dialect: "postgres",
       host: DB_HOST,
-      port: 5432,
+      port: DB_PORT,
       username: DB_USER,
       password: DB_PASSWORD,
       pool: {
@@ -23,7 +23,6 @@ let sequelize =
       dialectOptions: {
         ssl: {
           require: true,
-          // Ref.: https://github.com/brianc/node-postgres/issues/2009
           rejectUnauthorized: false,
         },
         keepAlive: true,
